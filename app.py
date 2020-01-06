ef73c7eb89ca91aedac9aa299566a27dc756e64b
Daniel Tierney <immortals11@hotmail.co.uk>
2020-01-06 20:43:31 +0000

    Base app and template working, being pulled through correctly to index

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'gametionary'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World! Again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("index.html", entries=mongo.db.entries.find())
 
 
 if __name__ == '__main__':
