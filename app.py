7ae29e2c4fe57a6d00f08e02524aabfb07d72e2c
Daniel Tierney <immortals11@hotmail.co.uk>
2020-01-20 12:38:30 +0000

    Upvotes field now submits as an integer on add and edit entry pages. This allows the upvotes function to now work correctly

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", entries=mongo.db.entries.find())
+    return render_template("index.html", entries=mongo.db.entries.aggregate([{'$sample': {'size': 2}}]))
 
 
 @app.route('/az')
 def az():
     return render_template("az.html", entries=mongo.db.entries.find().sort('name'))
-
+ 
 
 @app.route('/random')
 def random():
@@ -47,8 +47,10 @@ def add_entry():
 
 @app.route('/insert_entry', methods=['POST'])
 def insert_entry():
+    request_dict = request.form.to_dict()
+    request_dict['upvotes'] = 0
     entries = mongo.db.entries
-    entries.insert_one(request.form.to_dict())
+    entries.insert_one(request_dict)
     return redirect(url_for('add_entry'))
 
 
@@ -65,7 +67,7 @@ def update_entry(entry_id):
             {
                 'name': request.form.get('name'),
                 'description': request.form.get('description'),
-                'upvotes': request.form.get('upvotes')
+                'upvotes': int(request.form.get('upvotes'))
             })
     return redirect(url_for('index'))
 
@@ -83,24 +85,24 @@ def add_upvote(entry_id):
     return redirect(url_for('index'))
 
 
-@app.route('/validator')
-def validator():
-    return mongo.db.runCommand({'collMod': 'entries',
-                                'validator': {'$jsonSchema': {
-                                    'bsonType': 'object',
-                                    'properties': {
-                                        'name': {
-                                            'bsonType': 'string'
-                                        },
-                                        'description': {
-                                            'bsonType': 'string'
-                                        },
-                                        'upvotes': {
-                                            'bsonType': 'int32'
-                                        }
-                                    }
-                                }}, 'validationLevel': 'strict'
-                                })
+# @app.route('/validator')
+# def validator():
+#     return mongo.db.runCommand({'collMod': 'entries',
+#                                 'validator': {'$jsonSchema': {
+#                                     'bsonType': 'object',
+#                                     'properties': {
+#                                         'name': {
+#                                             'bsonType': 'string'
+#                                         },
+#                                         'description': {
+#                                             'bsonType': 'string'
+#                                         },
+#                                         'upvotes': {
+#                                             'bsonType': 'int32'
+#                                         }
+#                                     }
+#                                 }}, 'validationLevel': 'strict'
+#                                 })
 
 
 if __name__ == '__main__':
